ce05172a53e6140b8dbcc46ee89ddd737e39efea
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-24 22:47:16 +0600

    Update user-generator.ts

--- lib/user-generator.ts
+++ lib/user-generator.ts
@@ -1,60 +1,63 @@
-import { faker as baseFaker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
+import { fakerEN, fakerFR } from '@faker-js/faker/locales'; // Import specific locales
 
 export interface UserFields {
-  id: boolean
-  firstName: boolean
-  lastName: boolean
-  email: boolean
-  phone: boolean
-  address: boolean
-  city: boolean
-  country: boolean
-  zipCode: boolean
-  birthDate: boolean
-  age: boolean
-  username: boolean
-  password: boolean
-  avatar: boolean
-  company: boolean
-  jobTitle: boolean
+  id: boolean;
+  firstName: boolean;
+  lastName: boolean;
+  email: boolean;
+  phone: boolean;
+  address: boolean;
+  city: boolean;
+  country: boolean;
+  zipCode: boolean;
+  birthDate: boolean;
+  age: boolean;
+  username: boolean;
+  password: boolean;
+  avatar: boolean;
+  company: boolean;
+  jobTitle: boolean;
 }
 
 export interface GenerateOptions {
-  quantity: number
-  fields: UserFields
-  gender: 'male' | 'female' | 'random'
-  country: string | null
+  quantity: number;
+  fields: UserFields;
+  gender: 'male' | 'female' | 'random';
+  country: string | null; // Use locale strings such as 'en', 'fr', etc.
 }
 
 export function generateUsers(options: GenerateOptions) {
-  const { quantity, fields, gender, country } = options
-  const faker = country ? baseFaker.locale(country) : baseFaker // Create a localized faker instance if a country is specified
+  const { quantity, fields, gender, country } = options;
+
+  // Use the correct locale
+  const fakerInstance = country === 'fr' ? faker.fakerFR : faker.fakerEN;
 
   return Array.from({ length: quantity }, () => {
-    const userGender = gender === 'random' ? (Math.random() > 0.5 ? 'male' : 'female') : gender
+    const userGender = gender === 'random' ? (Math.random() > 0.5 ? 'male' : 'female') : gender;
 
-    const user: any = {}
+    const user: any = {};
 
-    if (fields.id) user.id = faker.string.uuid()
-    if (fields.firstName) user.firstName = faker.person.firstName(userGender)
-    if (fields.lastName) user.lastName = faker.person.lastName(userGender)
-    if (fields.email) user.email = faker.internet.email({ firstName: user.firstName, lastName: user.lastName })
-    if (fields.phone) user.phone = faker.phone.number()
-    if (fields.address) user.address = faker.location.streetAddress()
-    if (fields.city) user.city = faker.location.city()
-    if (fields.country) user.country = faker.location.country()
-    if (fields.zipCode) user.zipCode = faker.location.zipCode()
+    if (fields.id) user.id = fakerInstance.string.uuid();
+    if (fields.firstName) user.firstName = fakerInstance.person.firstName(userGender);
+    if (fields.lastName) user.lastName = fakerInstance.person.lastName(userGender);
+    if (fields.email) user.email = fakerInstance.internet.email({ firstName: user.firstName, lastName: user.lastName });
+    if (fields.phone) user.phone = fakerInstance.phone.number();
+    if (fields.address) user.address = fakerInstance.location.streetAddress();
+    if (fields.city) user.city = fakerInstance.location.city();
+    if (fields.country) user.country = fakerInstance.location.country();
+    if (fields.zipCode) user.zipCode = fakerInstance.location.zipCode();
     if (fields.birthDate) {
-      const birthDate = faker.date.birthdate()
-      user.birthDate = birthDate.toISOString().split('T')[0]
-      if (fields.age) user.age = new Date().getFullYear() - birthDate.getFullYear()
+      const birthDate = fakerInstance.date.birthdate();
+      user.birthDate = birthDate.toISOString().split('T')[0];
+      if (fields.age) user.age = new Date().getFullYear() - birthDate.getFullYear();
     }
-    if (fields.username) user.username = faker.internet.userName({ firstName: user.firstName, lastName: user.lastName })
-    if (fields.password) user.password = faker.internet.password()
-    if (fields.avatar) user.avatar = faker.image.avatar()
-    if (fields.company) user.company = faker.company.name()
-    if (fields.jobTitle) user.jobTitle = faker.person.jobTitle()
+    if (fields.username) user.username = fakerInstance.internet.userName({ firstName: user.firstName, lastName: user.lastName });
+    if (fields.password) user.password = fakerInstance.internet.password();
+    if (fields.avatar) user.avatar = fakerInstance.image.avatar();
+    if (fields.company) user.company = fakerInstance.company.name();
+    if (fields.jobTitle) user.jobTitle = fakerInstance.person.jobTitle();
 
-    return user
-  })
+    return user;
+  });
 }
