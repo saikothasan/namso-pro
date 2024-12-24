9e1b94f9fba8d40070f9e36fa50ee3f388650a60
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-24 23:00:23 +0600

    Update user-generator.ts

--- lib/user-generator.ts
+++ lib/user-generator.ts
@@ -23,38 +23,40 @@ export interface GenerateOptions {
   quantity: number
   fields: UserFields
   gender: 'male' | 'female' | 'random'
-  locale?: string // Added locale type here
+  locale?: string // Use this instead of 'country' for faker.locale
 }
 
 export function generateUsers(options: GenerateOptions) {
-  const { quantity, fields, gender, locale } = options
+  const { quantity, fields, gender, locale } = options;
+
+  // Set faker locale
+  if (locale) faker.locale = locale;
 
   return Array.from({ length: quantity }, () => {
-    faker.locale = locale || faker.locale
-    const userGender = gender === 'random' ? (Math.random() > 0.5 ? 'male' : 'female') : gender
-    
-    const user: any = {}
-    
-    if (fields.id) user.id = faker.string.uuid()
-    if (fields.firstName) user.firstName = faker.person.firstName(userGender)
-    if (fields.lastName) user.lastName = faker.person.lastName(userGender)
-    if (fields.email) user.email = faker.internet.email({ firstName: user.firstName, lastName: user.lastName })
-    if (fields.phone) user.phone = faker.phone.number()
-    if (fields.address) user.address = faker.location.streetAddress()
-    if (fields.city) user.city = faker.location.city()
-    if (fields.country) user.country = faker.location.country()
-    if (fields.zipCode) user.zipCode = faker.location.zipCode()
+    const userGender = gender === 'random' ? (Math.random() > 0.5 ? 'male' : 'female') : gender;
+
+    const user: any = {};
+
+    if (fields.id) user.id = faker.string.uuid();
+    if (fields.firstName) user.firstName = faker.person.firstName(userGender);
+    if (fields.lastName) user.lastName = faker.person.lastName(userGender);
+    if (fields.email) user.email = faker.internet.email({ firstName: user.firstName, lastName: user.lastName });
+    if (fields.phone) user.phone = faker.phone.number();
+    if (fields.address) user.address = faker.location.streetAddress();
+    if (fields.city) user.city = faker.location.city();
+    if (fields.country) user.country = faker.location.country();
+    if (fields.zipCode) user.zipCode = faker.location.zipCode();
     if (fields.birthDate) {
-      const birthDate = faker.date.birthdate()
-      user.birthDate = birthDate.toISOString().split('T')[0]
-      if (fields.age) user.age = new Date().getFullYear() - birthDate.getFullYear()
+      const birthDate = faker.date.birthdate();
+      user.birthDate = birthDate.toISOString().split('T')[0];
+      if (fields.age) user.age = new Date().getFullYear() - birthDate.getFullYear();
     }
-    if (fields.username) user.username = faker.internet.userName({ firstName: user.firstName, lastName: user.lastName })
-    if (fields.password) user.password = faker.internet.password()
-    if (fields.avatar) user.avatar = faker.image.avatar()
-    if (fields.company) user.company = faker.company.name()
-    if (fields.jobTitle) user.jobTitle = faker.person.jobTitle()
-    
-    return user
-  })
+    if (fields.username) user.username = faker.internet.userName({ firstName: user.firstName, lastName: user.lastName });
+    if (fields.password) user.password = faker.internet.password();
+    if (fields.avatar) user.avatar = faker.image.avatar();
+    if (fields.company) user.company = faker.company.name();
+    if (fields.jobTitle) user.jobTitle = faker.person.jobTitle();
+
+    return user;
+  });
 }
